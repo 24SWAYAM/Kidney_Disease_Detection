75bd7d8159d2b85f9488453e77020acf27c36e18
24SWAYAM <swayamsoni24@gmail.com>
2024-07-03 13:27:24 +0530

    Evaluation and mlflow added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from CNN_classifier import logger
 from CNN_classifier.pipeline.state_01_data_ingestion import DataIngestionTrainingPipeline
 from CNN_classifier.pipeline.stage_02_base_model import PrepareBaseModelTrainingPipeline
 from CNN_classifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+from CNN_classifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -45,3 +46,19 @@ if __name__ == '__main__':
     except Exception as e:
         logger.exception(e)
         raise e
+    
+
+STAGE_NAME = "Evaluation stage"
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+            
--- src/CNN_classifier/config/configuration.py
+++ src/CNN_classifier/config/configuration.py
@@ -1,7 +1,7 @@
 
 from CNN_classifier.constants import *
 from CNN_classifier.utils.common import read_yaml, create_directories
-from CNN_classifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig
+from CNN_classifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig,EvaluationConfig
 import os
 class ConfigurationManager:
     def __init__(
@@ -72,3 +72,14 @@ class ConfigurationManager:
         )
 
         return training_config
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/CT-KIDNEY-DATASET-Normal-Cyst-Tumor-Stone",
+            mlflow_uri="https://dagshub.com/24SWAYAM/Kidney_Disease_Detection.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
